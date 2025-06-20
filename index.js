fd52e11e718b872e331225f1fdace2cb4da3d8fb
SIAM632713 <siamrezwanahmed@gmail.com>
2025-06-21 02:19:08 +0600

    f15

--- index.js
+++ index.js
@@ -11,18 +11,10 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = ['http://localhost:5173', 'https://news-portal-psi-two.vercel.app'];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: "http://localhost:5173",
     credentials: true,
-}));
+}))
 
 export const JWT_SECRET = process.env.JWT_SECRET_KEY;
 
