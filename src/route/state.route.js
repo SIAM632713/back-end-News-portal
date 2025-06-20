48c289a8403dd09be6e05ddfcbbc142590e990f3
SIAM632713 <siamrezwanahmed@gmail.com>
2025-06-21 02:34:47 +0600

    f21

--- src/route/state.route.js
+++ src/route/state.route.js
@@ -5,6 +5,6 @@ import {verifyAdmin} from "../middleware/verifyAdmin.js";
 
 const router = express.Router();
 
-router.get("/admin-state",verifyToken,verifyAdmin,adminState)
+router.get("/admin-state",verifyAdmin,adminState)
 
 export default router
