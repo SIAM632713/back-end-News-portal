183b97ef358f6e4843ed297c710928289af82a58
SIAM632713 <siamrezwanahmed@gmail.com>
2025-06-22 01:50:07 +0600

    f55

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ import uploadImage from "./src/utilitis/uploadImage.js";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://news-portal-roan.vercel.app"],
     credentials: true,
 }));
 app.use(express.json());
