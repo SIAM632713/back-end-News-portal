3792cd5dc67ccfcde3b1864249cee4976e72de20
SIAM632713 <siamrezwanahmed@gmail.com>
2025-06-21 15:49:45 +0600

    f14

--- src/route/article.route.js
+++ src/route/article.route.js
@@ -15,6 +15,6 @@ router.get("/getall-post",getAllArticles)
 router.get("/get-query",getArticleQuery)
 router.get("/getsingle-post/:id",getSingleArticle)
 router.post("/update-post/:id",verifyToken,verifyAdmin,updateArticle)
-router.delete("/delete-post/:id",deleteArticle)
+router.delete("/delete-post/:id",verifyToken,verifyAdmin,deleteArticle)
 
 export default router;
